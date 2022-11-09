f9e32f72321a458fb801f0b5e6faecf1cfb2c213
ssgadient <sgadient@ucsd.edu>
2022-11-09 15:36:01 -0800

    update

--- TestListExamples.java
+++ TestListExamples.java
@@ -1,6 +1,31 @@
 import static org.junit.Assert.*;
 import org.junit.*;
 
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+class containsLetterX implements StringChecker {
+
+  public boolean checkString(String s) {
+    return s.contains("x") || s.contains("X");
+  }
+
+}
+
 public class TestListExamples {
-  // Write your grading tests here!
+  
+  @Test
+    public void testFilter() {
+      List<String> input1 = new ArrayList<String>(Arrays.asList("xylophone", "apples", "", "XxXx", "    x "));
+      assertEquals(new ArrayList<String>(Arrays.asList("xylophone", "XxXx", "    x ")), ListExamples.filter(input1, new containsLetterX()));
+    }
+
+  @Test
+    public void testMerge() {
+      List<String> input1 = new ArrayList<String>(Arrays.asList("a", "c", "e"));
+      List<String> input2 = new ArrayList<String>(Arrays.asList("b", "d", "f"));
+      List<String> output = new ArrayList<String>(Arrays.asList("a", "b", "c", "d", "e", "f"));
+      assertEquals(output, ListExamples.merge(input1, input2));
+    }
 }
--- grade.sh
+++ grade.sh
@@ -1,7 +1,42 @@
 # Create your grading script here
 
-set -e
+$CP=".;lib/junit-4.13.2.jar;lib/hamcrest-core-1.3.jar"
+$error=0
 
 rm -rf student-submission
 git clone $1 student-submission
+echo "Cloned student submission!"
+
+javac -cp $CP *.java
+
+if [ $? -ne 0 ]
+then
+    echo "Compiler error!"
+    exit 1
+fi
+
+java -cp $CP org.junit.runner.JUnitCore TestListExamples 2> error.txt
+
+if [ grep -c "testFilter" error.txt -ne 0 ]
+then
+    $error += 1
+    echo "testFilter failed!"
+fi
+
+if [ grep -c "testMerge" error.txt -ne 0 ]
+then
+    $error += 1
+    echo "testMerge failed!"
+fi
+
+if [ $error -eq 2 ]
+then
+    echo "Score: 0/2"
+elif [ $error -eq 1 ]
+then
+    echo "Score: 1/2"
+elif [ $error -eq 0 ]
+then
+    echo "Score: 2/2"
+fi
 
