34487830145066aa099ddb14af3058c6197a660b
ssgadient <sgadient@ucsd.edu>
2022-11-16 14:52:48 -0800

    update

--- grade.sh
+++ grade.sh
@@ -4,6 +4,7 @@ rm -rf student-submission
 git clone $1 student-submission
 echo "Cloned student submission!"
 error=0
+javac -cp ".:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar" TestListExamples.java
 CP="..:../lib/hamcrest-core-1.3.jar:../lib/junit-4.13.2.jar"
 cd student-submission
 
