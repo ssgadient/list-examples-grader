8e2906ae4e45b678ed132ee54028bb092cc2ebe0
ssgadient <ssgadient@gmail.com>
2022-11-28 12:22:58 -0800

    update

--- grade.sh
+++ grade.sh
@@ -4,6 +4,7 @@ git clone $1 student-submission 2> /dev/null
 echo "Done!"
 error=0
 javac -target 1.8 -cp ".:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar" ListExamples.java TestListExamples.java
+rm ListExamples.class
 CP="..:../lib/hamcrest-core-1.3.jar:../lib/junit-4.13.2.jar"
 cd student-submission
 
